7fffb3c7a0639546ba065ab3dc12245dbc41403c
matthwize <matthias@aureliusgroup.se>
2026-03-14 18:39:18 +0100

    Update superfunnel.php

--- superfunnel.php
+++ superfunnel.php
@@ -3,8 +3,8 @@
  * Plugin Name: Superfunnel
  * Plugin URI: https://example.com
  * Description: Lågvikts funnel-plugin med sessions, kvalificerade besök, dropoff och adminöversikt. Integrerar Meta/Google för ROAS/POAS.
- * Version: 4.3.7
- * Author: Matthias
+ * Version: 4.3.9
+ * Author: Aurelius Group Sweden
  * Requires at least: 6.0
  * Requires PHP: 7.4 
  */
@@ -36,7 +36,7 @@ if (file_exists($checkerPath)) {
     }
 }
 
-define('SUPERFUNNEL_VERSION', '4.3.7');
+define('SUPERFUNNEL_VERSION', '4.3.9');
 define('SUPERFUNNEL_PLUGIN_FILE', __FILE__);
 define('SUPERFUNNEL_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('SUPERFUNNEL_PLUGIN_URL', plugin_dir_url(__FILE__));
